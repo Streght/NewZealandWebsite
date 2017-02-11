d28099114db336a014ce11246b3386c0d26fd9a7
Nicolas Gilbert <n-gilbert@laposte.net>
2017-02-11 18:52:08 -0500

    Clean CCS folder Add following pages : presentation, histoire, sport, infos, à propos

--- src/Uniteam/PresentationBundle/Controller/PagesController.php
+++ src/Uniteam/PresentationBundle/Controller/PagesController.php
@@ -9,25 +9,24 @@ class PagesController extends Controller {
     public function pageAction($page) {
 
         if ($page == "presentation") {
-            return $this->render('UniteamPresentationBundle::presentation.html.twig');
+            return $this->render('UniteamPresentationBundle:Pages:presentation.html.twig', array('currentpage' => 'presentation'));
         }
         if ($page == "histoire") {
             return $this->render('UniteamPresentationBundle:Pages:histoire.html.twig', array('currentpage' => 'histoire'));
         }
         if ($page == "photos") {
-            return $this->render('UniteamPresentationBundle:Pages:histoire.html.twig', array('currentpage' => 'photos'));
+            return $this->render('UniteamPresentationBundle:Pages:photos.html.twig', array('currentpage' => 'photos'));
         }
         if ($page == "sport") {
-            return $this->render('UniteamPresentationBundle:Pages:histoire.html.twig', array('currentpage' => 'sport'));
+            return $this->render('UniteamPresentationBundle:Pages:sport.html.twig', array('currentpage' => 'sport'));
         }
         if ($page == "infos") {
-            return $this->render('UniteamPresentationBundle:Pages:histoire.html.twig', array('currentpage' => 'infos'));
+            return $this->render('UniteamPresentationBundle:Pages:infos.html.twig', array('currentpage' => 'infos'));
         }
         if ($page == "about") {
-            return $this->render('UniteamPresentationBundle:Pages:histoire.html.twig', array('currentpage' => 'about'));
+            return $this->render('UniteamPresentationBundle:Pages:about.html.twig', array('currentpage' => 'about'));
         } else {
             throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
         }
     }
-
 }
