08da1457c297620b0ab74da8051fbfd529bd504f
Nicolas Gilbert <n-gilbert@laposte.net>
2017-04-20 01:49:37 -0400

    Add 2 simple semantic web queries Add fetch infos from local DB if DBpedia is down Add daily update for local DB from DBpedia

--- src/Uniteam/PresentationBundle/Controller/PagesController.php
+++ src/Uniteam/PresentationBundle/Controller/PagesController.php
@@ -4,23 +4,23 @@ namespace Uniteam\PresentationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-include_once '/semsol/ARC2.php';
-use ARC2;
-
 class PagesController extends Controller {
 
-    public function pageAction($page) {
+    function updateDBIfNecessary() {
 
-        if ($page == "accueil") {
+        $em = $this->getDoctrine()->getManager();
 
-            $dbpconfig = array(
-                "remote_store_endpoint" => "http://dbpedia.org/sparql",
-            );
+        $qb = $em->createQueryBuilder();
+        $qb->select('count(date)')
+                ->from('UniteamPresentationBundle:dbupdatehistory', 'date')
+                ->where('date.lastUpdateTime > :last')
+                ->setParameter('last', new \DateTime('-1 day'), \Doctrine\DBAL\Types\Type::DATETIME);
+        $nbRecordInLast24h = $qb->getQuery()->getResult();
 
-            $store = ARC2::getRemoteStore($dbpconfig);
-
-            $query = '
-                PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        if ($nbRecordInLast24h[0]['1'] == 0) {
+            
+            $finished = true;
+            $prefix = 'PREFIX owl: <http://www.w3.org/2002/07/owl#>
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -29,18 +29,95 @@ class PagesController extends Controller {
                 PREFIX : <http://dbpedia.org/resource/>
                 PREFIX dbpedia2: <http://dbpedia.org/property/>
                 PREFIX dbpedia: <http://dbpedia.org/>
-                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-R
-                SELECT ?birthdate WHERE{
-                :Lionel_Messi dbpedia2:birthDate ?birthdate
-                }';
+                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>';
+            $connection = $em->getConnection();
+            $platform = $connection->getDatabasePlatform();
+            
+            $connection->executeUpdate($platform->getTruncateTableSQL('language', true));
 
-            $result = $store->query($query, 'rows');
+            $query = $prefix . '
+                SELECT ?label WHERE {
+                    :New_Zealand dbo:language ?langue.
+                    ?langue rdfs:label ?label
+                    FILTER (lang(?label) = "fr")
+                }
+            ';
 
+            $requestURL = 'http://dbpedia.org/sparql?'
+                    . 'query=' . urlencode($query)
+                    . '&format=json';
 
+            $resultFromDBPediaLanguages = json_decode(
+                    $this->request($requestURL), true);
+
+            if ($resultFromDBPediaLanguages == null) {
+                $finished = false;
+            } else {
+                foreach ($resultFromDBPediaLanguages['results']['bindings'] as $result) {
+                    $value = new \Uniteam\PresentationBundle\Entity\Language;
+                    $value->setName($result['label']['value']);
+                    $em->persist($value);
+                }
+            }
+
+            $connection->executeUpdate($platform->getTruncateTableSQL('city', true));
+            
+            $query = $prefix . '
+                SELECT DISTINCT ?name ?maxElevation WHERE {
+                    ?ville dbo:type :Urban_areas_of_New_Zealand .
+                    ?ville dbp:name ?name .
+                    ?ville dbo:maximumElevation ?maxElevation
+                }
+                ORDER BY DESC(?maxElevation)
+            ';
+
+            $requestURL = 'http://dbpedia.org/sparql?'
+                    . 'query=' . urlencode($query)
+                    . '&format=json';
+
+            $resultFromDBPediaCities = json_decode(
+                    $this->request($requestURL), true);
+            
+            if ($resultFromDBPediaCities == null) {
+                $finished = false;
+            } else {
+                foreach ($resultFromDBPediaCities['results']['bindings'] as $result) {
+                    $value = new \Uniteam\PresentationBundle\Entity\City;
+                    $value->setName($result['name']['value']);
+                    $value->setElevation($result['maxElevation']['value']);
+                    $em->persist($value);
+                }
+            }
+
+            if($finished == true) {
+                $timeOfUpdate = new \Uniteam\PresentationBundle\Entity\dbupdatehistory;
+                $timeOfUpdate->setLastUpdateTime(new \DateTime());
+                $em->persist($timeOfUpdate);
+
+                $em->flush();
+            }
+        }
+    }
+
+    function request($url) {
+        if (!function_exists('curl_init')) {
+            die('CURL is not installed!');
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+
+    public function pageAction($page) {
+
+        if ($page == "accueil") {
             return $this->render('UniteamPresentationBundle:Pages:accueil.html.twig', array(
-                        'currentpage' => 'accueil',
-                        'test' => $result
+                        'currentpage' => 'accueil'
             ));
         }
         if ($page == "presentation") {
@@ -73,19 +150,85 @@ R
                     ->getRepository('UniteamPresentationBundle:Country');
             $country = $repository->find(1);
 
-            $repository = $this->getDoctrine()
-                    ->getManager()
-                    ->getRepository('UniteamPresentationBundle:City');
-            $auckland = $repository->findOneByName('Auckland');
-            $christchurch = $repository->findOneByName('ChristChurch');
-            $wellington = $repository->findOneByName('Wellington');
+            $prefix = 'PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                PREFIX dc: <http://purl.org/dc/elements/1.1/>
+                PREFIX : <http://dbpedia.org/resource/>
+                PREFIX dbpedia2: <http://dbpedia.org/property/>
+                PREFIX dbpedia: <http://dbpedia.org/>
+                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>';
+            
+            $query = $prefix . '
+                SELECT DISTINCT ?name ?maxElevation WHERE {
+                    ?ville dbo:type :Urban_areas_of_New_Zealand .
+                    ?ville dbp:name ?name .
+                    ?ville dbo:maximumElevation ?maxElevation
+                }
+                ORDER BY DESC(?maxElevation)
+            ';
 
-            $repository = $this->getDoctrine()
-                    ->getManager()
-                    ->getRepository('UniteamPresentationBundle:Language');
-            $firstlang = $repository->find(1);
-            $secondlang = $repository->find(2);
-            $thirdlang = $repository->find(3);
+            $requestURL = 'http://dbpedia.org/sparql?'
+                    . 'query=' . urlencode($query)
+                    . '&format=json';
+
+            $resultFromDBPedia = json_decode(
+                    $this->request($requestURL), true);
+
+            $cities = array();
+
+            if ($resultFromDBPedia == null) {
+                $repository = $this->getDoctrine()
+                        ->getManager()
+                        ->getRepository('UniteamPresentationBundle:City');
+
+                $resultsFromDB = $repository->findAll();
+
+                foreach ($resultsFromDB as $result) {
+                    array_push($cities, array($result->getName(), $result->getElevation()));
+                }
+            } else {
+                foreach ($resultFromDBPedia['results']['bindings'] as $result) {
+                    array_push($cities, array($result['name']['value'], $result['maxElevation']['value']));
+                }
+            }
+
+            $query = $prefix . '
+                SELECT ?language WHERE {
+                    :New_Zealand dbo:language ?langue.
+                    ?langue rdfs:label ?language
+                    FILTER (lang(?language) = "fr")
+                }
+            ';
+
+            $requestURL = 'http://dbpedia.org/sparql?'
+                    . 'query=' . urlencode($query)
+                    . '&format=json';
+
+            $resultFromDBPedia = json_decode(
+                    $this->request($requestURL), true);
+
+            $languages = array();
+
+            if ($resultFromDBPedia == null) {
+                $repository = $this->getDoctrine()
+                        ->getManager()
+                        ->getRepository('UniteamPresentationBundle:Language');
+
+                $resultsFromDB = $repository->findAll();
+
+                foreach ($resultsFromDB as $result) {
+                    array_push($languages, array($result->getName()));
+                }
+            } else {
+                foreach ($resultFromDBPedia['results']['bindings'] as $result) {
+                    array_push($languages, array($result['language']['value']));
+                }
+            }
+
+            $this->updateDBIfNecessary();
 
             return $this->render('UniteamPresentationBundle:Pages:presentation.html.twig', array(
                         'currentpage' => 'presentation',
@@ -101,12 +244,8 @@ R
                         'pib' => $pib,
                         'pibparhab' => $pibperinhab,
                         'pays' => $country,
-                        'villeUn' => $auckland,
-                        'villeDeux' => $christchurch,
-                        'villeTrois' => $wellington,
-                        'langueUn' => $firstlang,
-                        'langueDeux' => $secondlang,
-                        'langueTrois' => $thirdlang
+                        'villes' => $cities,
+                        'langues' => $languages
             ));
         }
         if ($page == "photos") {
